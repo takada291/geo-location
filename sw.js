fabc9f7609309489b29a620adfa6c266d0826573
takada291 <tkp291@gmail.com>
2026-01-02 13:32:17 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,24 +1,25 @@
-const CACHE_NAME = 'geo-app-v1';
+const CACHE_NAME = 'geo-app-v2'; // ここをv1からv2に変えるだけで更新されます
 const urlsToCache = [
+  './',
   'index.html',
-  'manifest.json'
+  'manifest.json',
+  'sw.js',
+  'icon-192.png'
 ];
 
-// インストール時にファイルをキャッシュ
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(urlsToCache);
     })
   );
+  self.skipWaiting(); // すぐに新しい設定を適用する
 });
 
-// オフライン時はキャッシュからファイルを表示
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
       return response || fetch(event.request);
     })
   );
-
 });
