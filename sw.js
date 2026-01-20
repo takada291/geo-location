9248f7198199494305e41d7b453d130ca978b6fe
takada291 <tkp291@gmail.com>
2026-01-20 17:38:08 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'geo-location-v2.3.0'; //2026.01.19 18:28 
+const CACHE_NAME = 'geo-location-v2.5.0'; //2026.01.20 17:37 
 const urlsToCache = [
   './',
   'index.html',
@@ -67,6 +67,7 @@ self.addEventListener('fetch', (event) => {
     })
   );
 });
+
 
 
 
